6bc71f2bca28607db8d21c7e986a0574955597be
seigmann <aurora@skomsvold.net>
2023-11-03 12:00:34 +0100

    add flags finished (image name is lowercase name, can't add image from app atm)

--- Flagged/app/src/main/java/com/example/flagged/AddFlagActivity.kt
+++ Flagged/app/src/main/java/com/example/flagged/AddFlagActivity.kt
@@ -2,10 +2,10 @@ package com.example.flagged
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
 class AddFlagActivity : AppCompatActivity() {
@@ -20,12 +20,18 @@ class AddFlagActivity : AppCompatActivity() {
         val flagStockInput = findViewById<EditText>(R.id.stockInput)
         val flagDescriptionInput = findViewById<EditText>(R.id.descriptionInput)
         val flagCategoryInput = findViewById<Spinner>(R.id.categorySpinner)
+
+        val categories = arrayOf("Country", "Naval", "County/City", "Limited Edition", "LGBTQ+")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        flagCategoryInput.adapter = adapter
+
         val addButton = findViewById<AppCompatButton>(R.id.addButton)
 
         addButton.setOnClickListener{
             val name = flagNameInput.text.toString()
-            val price = flagPriceInput.text.toString()
-            val stock = flagStockInput.text.toString()
+            val price = flagPriceInput.text.toString().toIntOrNull() ?: 0
+            val stock = flagStockInput.text.toString().toIntOrNull() ?: 0
             val description = flagDescriptionInput.text.toString()
             val category = flagCategoryInput.selectedItem.toString()
 
@@ -35,10 +41,10 @@ class AddFlagActivity : AppCompatActivity() {
                 stock = stock,
                 description = description,
                 category = category,
-                image = " "
+                image = name.lowercase()
             )
             db.addFlag(flag)
-
+            finish()
         }
 
     }
